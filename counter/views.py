acb62e9bf5519c0dba8b0499f51ffd6f39ca666a
AlCapone0 <lsy83@mail.ru>
2018-12-27 22:50:20 +0800

    ver4

--- counter/views.py
+++ counter/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Companies
 
-# Create your views here.
+
+def companies_list(request):
+    companies = Companies.objects.all()
+    return render(request, "companies/companies_list.html", {"companies": companies})
+
+def company_detail(request, company_id):
+    company = get_object_or_404(Companies, id=company_id)
+    return render(request, "companies/company_detail.html", {"company": company})
--- sales_counter/urls.py
+++ sales_counter/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from counter.views import companies_list, company_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', companies_list, name='home'),
+    path('<company_id>/', company_detail, name='company'),
 ]
